7e241dbc546013a35909529912dd0029f3325eb5
Daniel Gallagher <daniel.gallagher.js@gmail.com>
2025-06-27 00:07:17 +0200

    updated

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import os
 
 st.set_page_config(page_title="", layout="wide")
 
-st.title("Sam & Dan's Architecture Map of Leipzig")
-st.write("A tour featuring the unusual mixture of Jugenstil and other types of architecture.")
+st.title("Sam & Dan's Quirky Architecture of (West) Leipzig")
 
 FILE_PATH = "./dataset.csv"
 
 # Function to convert image to base64
+@st.cache_data
 def get_base64_image(img_path):
     try:
         with open(img_path, "rb") as f:
@@ -20,33 +20,43 @@ def get_base64_image(img_path):
     except Exception as e:
         return ""
 
-df = pd.read_csv(FILE_PATH)
+@st.cache_data
+def load_data(path):
+    return pd.read_csv(path)
 
-required_cols = {'latitude', 'longitude', 'title', 'image_path'}
-if required_cols.issubset(df.columns):
-
+@st.cache_data
+def prepare_data(df):
     def generate_tooltip_html(row):
-        img_full_path = os.path.join("images", row["image_path"])
-        img_b64 = get_base64_image(img_full_path)
-        if img_b64:
-            img_html = f"<img src='data:image/jpeg;base64,{img_b64}' width='150'>"
-        else:
+        img_full_path = os.path.join("compressed", row["image_path"])
+        try:
+            with open(img_full_path, "rb") as f:
+                data = f.read()
+                img_b64 = base64.b64encode(data).decode()
+                img_html = f"<img src='data:image/jpeg;base64,{img_b64}' width='400'>"
+        except Exception as e:
             img_html = "<i>(image not found)</i>"
+
         return f"""
             <div style='text-align:left'>
                 <b>{row["title"]}</b><br>
                 {img_html}
             </div>
         """
-
     df["tooltip_html"] = df.apply(generate_tooltip_html, axis=1)
+    return df
 
-    st.subheader("Map Visualisation")
+df = load_data(FILE_PATH)
+required_cols = {'latitude', 'longitude', 'title', 'image_path'}
+if required_cols.issubset(df.columns):
+
+    df = prepare_data(df)
+
+    st.subheader("Map")
     st.pydeck_chart(pdk.Deck(
         initial_view_state=pdk.ViewState(
             latitude=51.333839,
             longitude=12.329561,
-            zoom=20,
+            zoom=13,
             pitch=0,
         ),
         layers=[
@@ -58,6 +68,16 @@ if required_cols.issubset(df.columns):
                 get_color='[255, 100, 100, 160]',
                 pickable=True,
             ),
+            pdk.Layer(
+                "TextLayer",
+                data=df,
+                get_position='[longitude, latitude]',
+                get_text="title",
+                get_size=12,
+                get_color=[0, 0, 0],
+                get_angle=0,
+                get_alignment_baseline="'bottom'",
+            )
         ],
         tooltip={"html": "{tooltip_html}", "style": {"backgroundColor": "white", "color": "black"}}
     ))
